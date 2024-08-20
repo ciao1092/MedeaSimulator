145a40c9aae6f972f5121e2af679bae950894218
Carlo <100089228+ciao1092@users.noreply.github.com>
2024-08-20 21:15:13 +1000

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: "docs",
+    distDir: "build",
     // to export static
     output: "export",
     images: {
