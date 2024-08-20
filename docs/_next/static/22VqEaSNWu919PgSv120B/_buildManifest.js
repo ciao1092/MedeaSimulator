1e349e6d2109dea3e0d62bb1f9f0690a8c69adba
Carlo <100089228+ciao1092@users.noreply.github.com>
2024-08-20 21:06:22 +1000

    Set up for gh pages

---
+++ docs/_next/static/22VqEaSNWu919PgSv120B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/675-7aa8406f586209f7.js","static/chunks/pages/index-edff137f470fcfbd.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: "build",
+    distDir: "docs",
     // to export static
     output: "export",
     images: {
